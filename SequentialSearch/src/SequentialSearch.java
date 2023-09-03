e144ec4a52e69bdaf1d5af9283438b650d529a52
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2023-09-03 20:17:17 -0300

    Added a string return if the value is found or not

--- SequentialSearch/src/SequentialSearch.java
+++ SequentialSearch/src/SequentialSearch.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class SequentialSearch {
     public static void main(String[] args) {
         Scanner valorInformado = new Scanner(System.in);
-
+        boolean achei = false;
         int[] vetor = new int[] { 10, 12, 45, 21, 65, 32, 78, 89, 13, 22 };
 
         System.out.println("Digite o valor a ser procurado:");
@@ -13,8 +13,14 @@ public class SequentialSearch {
             if (vetor[i] == valorBusca) {
                 System.out.println("Encontrado na posicao:" + (i + 1));
                 System.out.println("Valor: " + vetor[i]);
+                achei = true;
                 break;
             }
+        }
+        if (achei) {
+            System.out.println("Finalizado a busca sequencial do vetor e o valor foi encontrado");
+        } else {
+            System.out.println("O valor informado nao foi encontrado no vetor.");
         }
         valorInformado.close();
     }
