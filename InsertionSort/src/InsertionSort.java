44afb00e399fe9f105873d2a31ee524fbb279c92
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2023-09-03 22:41:39 -0300

    Added insertion sort algorithm

---
+++ InsertionSort/src/InsertionSort.java
@@ -0,0 +1,33 @@
+public class InsertionSort {
+    public static void main(String[] args) throws Exception {
+        int[] vetor = { 35, 4, 6, 8, 10, 70, 11, 64, 16, 75, 45, 3, 55, 7, 58, 5, 60, 66, 9 };
+        int valor;
+        int posAnterior;
+
+        for (int posAtual = 1; posAtual < vetor.length; posAtual++) {
+            valor = vetor[posAtual];
+            posAnterior = posAtual - 1;
+
+            mostraVetor(vetor);
+
+            while (posAnterior >= 0 && vetor[posAnterior] > valor) {
+                vetor[posAnterior + 1] = vetor[posAnterior];
+                posAnterior = posAnterior - 1;
+                System.out.println(posAnterior);
+            }
+            vetor[posAnterior + 1] = valor;
+        }
+        mostraVetor(vetor);
+    }
+
+    private static void mostraVetor(int[] v) {
+        System.out.print("Vetor \n[");
+        for (int i = 0; i < v.length; i++) {
+            System.out.print(v[i]);
+            if (i < v.length - 1)
+                System.out.print(',');
+
+        }
+        System.out.println("]");
+    }
+}
